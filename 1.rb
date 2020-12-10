c5d09779be497763f3e1fca02cb43aa2897b7c66
Andrii Palchik <andriypalchik@gmail.com>
2020-12-10 19:31:20 +0200

    Add files via upload
    Main

---
+++ 1.rb
@@ -0,0 +1,7 @@
+# Створити змінні $text1='Hello' и $text2='World'.
+# За допомогою цих змінних і операції складання рядків
+# вивести на екран фразу «Hello world».
+
+text1 = "Hello"
+text2 = "World"
+puts text1 + " " + text2
