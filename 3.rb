e5a08cce6f4a5813052047c63337765d8f845a47
Andrii Palchik <andriypalchik@gmail.com>
2021-01-18 10:08:44 +0200

    Update 3.rb

--- 3.rb
+++ 3.rb
@@ -44,39 +44,39 @@ price: 8.18
 
 books = [
     {    
-    name: 'Learning ruby, mysql and JavaScript',    
-    author: 'Robin Snow',    
-    price: 30,    
-    tags: ['ruby', 'javascript', 'mysql']    
+        name: 'Learning ruby, mysql and JavaScript',    
+        author: 'Robin Snow',    
+        price: 30,    
+        tags: ['ruby', 'javascript', 'mysql']    
     },    
     {    
-    name: 'Ruby for the Web: Visual QuickStart Guide',    
-    author: 'Larry Snow',    
-    price: 25,    
-    tags: ['ruby']    
+        name: 'Ruby for the Web: Visual QuickStart Guide',    
+        author: 'Larry Snow',    
+        price: 25,    
+        tags: ['ruby']    
     },    
     {    
-    name: 'Ruby and MySqL for Dynamic Web Sites',    
-    author: 'Larry Snow',    
-    price: 14.39,    
-    tags: ['ruby', 'mysql']    
+        name: 'Ruby and MySqL for Dynamic Web Sites',    
+        author: 'Larry Snow',    
+        price: 14.39,    
+        tags: ['ruby', 'mysql']    
     },    
     {    
-    name: 'Modern Ruby: New Features and Good Practices',    
-    author: 'Josh Snow',    
-    price: 24,    
-    tags: ['ruby']    
+        name: 'Modern Ruby: New Features and Good Practices',    
+        author: 'Josh Snow',    
+        price: 24,    
+        tags: ['ruby']    
     },    
     {    
-    name: 'JavaScript and JQuery: Interactive Front-End Web Development',    
-    author: 'Jon Snow',    
-    price: 20,    
-    tags: ['javascript', 'jquery']    
+        name: 'JavaScript and JQuery: Interactive Front-End Web Development',    
+        author: 'Jon Snow',    
+        price: 20,    
+        tags: ['javascript', 'jquery']    
     },    
     {    
-    name: 'Miss Peregrine Home for Peculiar Children',    
-    author: 'Ransom Snow',    
-    price: 8.18    
+        name: 'Miss Peregrine Home for Peculiar Children',    
+        author: 'Ransom Snow',    
+        price: 8.18    
     }    
 ]
 
