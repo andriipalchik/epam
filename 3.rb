45fdedda9e777e708e4bcd37f7b88d605d952d2d
Andrii Palchik <andriypalchik@gmail.com>
2021-01-18 10:19:10 +0200

    Update 3.rb

--- 3.rb
+++ 3.rb
@@ -1,11 +1,14 @@
 #Є масив a = [342, 55, 33, 123, 66, 63, 9]; 
 # - потрібно вивести на екран тільки ті числа в яких є '3'
 
-a = [342, 55, 33, 123, 66, 63, 9]
-b = []
-for i in a
+a == array
+b == match_array
+
+array = [342, 55, 33, 123, 66, 63, 9]
+match_array = []
+for i in array
     if i.to_s.include?('3')
-        b << i
+        match_array << i
     end
 end
-puts b
+puts match_array
